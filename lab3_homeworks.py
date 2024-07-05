86365485648a01ea75ba4bf96e88b054db63afec
HAMB787 <sah.hamb18@gmail.com>
2024-07-05 13:47:16 +0400

    added new files

---
+++ lab3_homeworks.py
@@ -0,0 +1,35 @@
+#1 A.Гипотенуза
+
+a=int(input())
+b=int(input())
+print((a*a + b*b)**0.5)
+
+#2 B.Следующее и предыдущее
+
+n=int(input())
+print(f'The next number of {n} is {n+1}')
+print(f'The  previuos number of {n} is {n-1}')
+
+#3 C.Дележ яблок - 1
+N=int(input()) #3
+K=int (input()) #14
+print(K//N)
+
+#4  D.Дележ яблок - 2
+
+N=int(input())
+K=int(input())
+print(K%N)
+
+#5 E.МКАД
+v=int(input())
+t=int(input())    
+print(v*t % 109)
+
+#6 F.Последняя цифра
+a=int(input())
+print(a%10)
+
+
+
+
